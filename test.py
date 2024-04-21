a7a578be530f2c493e3e2adbfd3fcf7e4b669b1a
Andri-Widiyanto12 <andriwdynt27@gmail.com>
2024-04-21 11:34:08 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,17 @@
+# first_name = 'jeane'
+# last_name = 'victoria'
+# full_name = first_name + ' ' + last_name
+# print(full_name)
+
+# first_name = 'jeane'
+# last_name = 'victoria'
+# full_name = f'{first_name} {last_name}'
+# print(full_name)
+
+# from datetime import datetime
+
+# today = datetime.now()
+# # yesterday = datetime.yesterday()  cannot use this code!!
+
+# date_time = today.strftime('%Y-%m-%d %H:%M:%S')
+# print(date_time)
